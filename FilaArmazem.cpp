9e10ca69de7b4a3ec7b18c40fd055bc6000dba61
QuantumCenS <xsadsadsadsadsadx@gmail.com>
2026-05-02 23:11:04 +0100

    Alteracoes feitas de acordo com que foi dado na PL

---
+++ FilaArmazem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sergio on 5/2/2026.
+//
+
+#include "FilaArmazem.h"
---
+++ FilaArmazem.h
@@ -0,0 +1,13 @@
+//
+// Created by Sergio on 5/2/2026.
+//
+
+#ifndef PROJETO2EDA_FILAARMAZEM_H
+#define PROJETO2EDA_FILAARMAZEM_H
+
+
+class FilaArmazem {
+};
+
+
+#endif //PROJETO2EDA_FILAARMAZEM_H
